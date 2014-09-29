fa356fb3e798f9f39047a645a542e1bec7f1c5fa
Josh Cornelius <joshcornelius9@gmail.com>
2014-09-29 12:22:55 -0400

    finished app

---
+++ app.js
@@ -0,0 +1,99 @@
+angular
+	.module('Eggly', [])
+	.controller('MainController', function($scope) {
+		$scope.categories = [
+        {"id": 0, "name": "Development"},
+        {"id": 1, "name": "Design"},
+        {"id": 2, "name": "Exercise"},
+        {"id": 3, "name": "Humor"}
+    ];
+
+	  $scope.bookmarks = [
+	      {"id": 0, "title": "AngularJS", "url": "http://angularjs.org", "category": "Development" },
+	      {"id": 1, "title": "Egghead.io", "url": "http://angularjs.org", "category": "Development" },
+	      {"id": 2, "title": "A List Apart", "url": "http://alistapart.com/", "category": "Design" },
+	      {"id": 3, "title": "One Page Love", "url": "http://onepagelove.com/", "category": "Design" },
+	      {"id": 4, "title": "MobilityWOD", "url": "http://www.mobilitywod.com/", "category": "Exercise" },
+	      {"id": 5, "title": "Robb Wolf", "url": "http://robbwolf.com/", "category": "Exercise" },
+	      {"id": 6, "title": "Senor Gif", "url": "http://memebase.cheezburger.com/senorgif", "category": "Humor" },
+	      {"id": 7, "title": "Wimp", "url": "http://wimp.com", "category": "Humor" },
+	      {"id": 8, "title": "Dump", "url": "http://dump.com", "category": "Humor" }
+	  ];
+
+	  $scope.currentCategory = null;
+	  $scope.isCreating = false;
+	  $scope.isEditing = false;
+
+	  $scope.setCurrentCategory = function(category) {
+	  	$scope.currentCategory = category;
+
+	  	$scope.cancelCreating();
+	  	$scope.cancelEditing();
+	  };
+
+	  $scope.isCurrentCategory = function(category) {
+	  	return $scope.currentCategory != null && $scope.currentCategory.name == category.name
+	  };
+
+	  $scope.startCreating = function() {
+	  	$scope.isCreating = true;
+	  	$scope.isEditing = false;
+
+	  	$scope.resetCreateForm();
+	  };
+
+	  $scope.cancelCreating = function() {
+	  	$scope.isCreating = false;
+	  };
+
+	  $scope.startEditing = function() {
+	  	$scope.isEditing = true;
+	  	$scope.isCreating = false;
+	  };
+
+	  $scope.cancelEditing = function() {
+	  	$scope.isEditing = false;
+	  };
+
+	  $scope.shouldShowCreating = function() {
+	  	return $scope.currentCategory && !$scope.isEditing;
+	  };
+
+	  $scope.shouldShowEditing = function() {
+	  	return $scope.isEditing && !$scope.isCreating;
+	  };
+
+	  $scope.resetCreateForm = function() {
+	  	$scope.newBookmark = {
+	  		title: '',
+	  		url: '',
+	  		category: $scope.currentCategory
+	  	};
+	  };
+
+	  $scope.createBookmark = function(bookmark) {
+	  	bookmark.id = $scope.bookmarks.length;
+	  	$scope.bookmarks.push(bookmark);
+
+	  	$scope.resetCreateForm();
+	  };
+
+	  $scope.editedBookmark = null;
+
+	  $scope.setEditedBookmark = function(bookmark) {
+	  	$scope.editedBookmark = angular.copy(bookmark);
+	  };
+
+	  $scope.updateBookmark = function(bookmark) {
+	  	var index = _.findIndex($scope.bookmarks, function(b) {
+	  		return b.id == bookmark.id;
+	  	});
+
+	  	$scope.bookmarks[index] = bookmark;
+	  	$scope.editedBookmark = null;
+	  	$scope.isEditing = false;
+	  };
+
+
+
+	});
